0e6bdaab2eb66966d57c445d892d302d7923cb31
Jaynil Bhalani <bhalanijaynil@gmail.com>
2026-02-26 15:21:43 +0530

    Update ShopKaro_One.py

--- ShopKaro_One.py
+++ ShopKaro_One.py
@@ -29,7 +29,7 @@ def db():
 # ---------- HOME ----------
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("Home.html")
 
 # ---------- SETUP CLICK ----------
 @app.route("/setup")
@@ -131,3 +131,4 @@ def callback():
 # ---------- RUN ----------
 if __name__ == "__main__":
     app.run(debug=True)
+
