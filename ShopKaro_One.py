aa7bd7c8e822e6ea66997b14dce8cfbd1ade56b3
Jaynil Bhalani <bhalanijaynil@gmail.com>
2026-02-28 12:17:11 +0530

    Update ShopKaro_One.py

--- ShopKaro_One.py
+++ ShopKaro_One.py
@@ -7,10 +7,11 @@ payment=True
 def home():
     if not payment:
         return render_template("a.html")
-    return redirect("https://shopkaro-6qlr.onrender.com")
+    return redirect("https://shopkaro-6qlr.onrender.com/")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
+
 
 
 
