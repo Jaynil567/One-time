0da3f68f2152df5cc8998494b996a9c6ba4b94f7
Jaynil Bhalani <bhalanijaynil@gmail.com>
2026-02-28 12:08:13 +0530

    Update ShopKaro_One.py

--- ShopKaro_One.py
+++ ShopKaro_One.py
@@ -1,11 +1,15 @@
-from flask import Flask, redirect
+from flask import Flask, redirect,render_template
 
 app = Flask(__name__)
 
+payment=False
 @app.route("/")
 def home():
+    if not payment:
+        return render_template("a.html")
     return redirect("https://shopkaro-6qlr.onrender.com")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
+
 
